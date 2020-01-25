8b32ecf42f45a8fd4cb18bdec1f82915a317f92d
mabrown303 <60299372+mabrown303@users.noreply.github.com>
2020-01-25 13:51:21 -0700

    Innitial Commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,53 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.TimedRobot;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class, specifically
+ * it contains the code necessary to operate a robot with tank drive.
+ */
+public class Robot extends TimedRobot {
+  private Joystick gamepad;
+  private VictorSPX flMotor;
+  private VictorSPX frMotor;
+  private VictorSPX blMotor;
+  private VictorSPX brMotor;
+  private double speed;
+  
+
+
+  @Override
+  public void robotInit() {
+    gamepad = new Joystick(0);
+    flMotor = new VictorSPX(1);
+    frMotor = new VictorSPX(4);
+    blMotor = new VictorSPX(2);
+    brMotor = new VictorSPX(3);
+    blMotor.follow(flMotor);
+    brMotor.follow(frMotor);
+    speed = .5;
+  }
+
+  @Override
+  public void teleopPeriodic() {
+   // m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+   frMotor.set(ControlMode.PercentOutput,gamepad.getRawAxis(5)*speed);
+   flMotor.set(ControlMode.PercentOutput,-gamepad.getRawAxis(1)*speed);
+   SmartDashboard.putNumber("Axis Data",gamepad.getRawAxis(5));
+  }
+}
+    
